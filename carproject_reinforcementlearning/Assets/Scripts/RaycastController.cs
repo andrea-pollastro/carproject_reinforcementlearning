23c8205521cb0ee7be4578adeda96d850a935324
andrea.pollastro <andrea.poll93@gmail.com>
2019-07-05 01:12:43 +0200

    QLearner class added
    Starting to write the q-learning method

---
+++ carproject_reinforcementlearning/Assets/Scripts/RaycastController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastController : MonoBehaviour
+{
+    public float maxLenghtRay = 25;
+    private Ray[] rays = new Ray[5];
+    //Oggetto necessario per trovare l'intersezione dei raggi con la scena
+    private RaycastHit hit;
+    private byte[] currentState = new byte[5];
+
+
+    public byte[] getCurrentState(byte[] qlearnerState)
+    {
+        for (byte i = 0; i < currentState.Length; i++)
+            qlearnerState[i] = currentState[i];
+        return qlearnerState;
+    }
+    //callback pre-frame
+    void FixedUpdate()
+    {
+        //Angolo di apertura totale: 120
+        int angle = -40;
+
+        //Creazione raggi
+        for(byte i = 0; i < rays.Length; i++){
+            rays[i] = new Ray(transform.position, Quaternion.AngleAxis(angle, Vector3.up) * transform.forward);
+            angle += 20;
+            currentState[i] = 3;
+        }
+
+        //Disegno raggi
+        foreach(Ray ray in rays)
+            Debug.DrawRay(ray.origin, ray.direction * maxLenghtRay, Color.green);
+        
+        //Raycasting
+        for(byte i = 0; i < rays.Length; i++){
+            if (Physics.Raycast(rays[i], out hit, maxLenghtRay)){
+                Vector3 res = hit.point - transform.position;
+                if (res.magnitude < 4)
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.red);
+                    currentState[i] = 0;
+                }
+                else if (res.magnitude < 8)
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.HSVToRGB((float).108, 1, 1));
+                    currentState[i] = 1;
+                }
+                else
+                {
+                    Debug.DrawLine(transform.position, hit.point, Color.yellow);
+                    currentState[i] = 2;
+                }
+            }
+        }
+    }
+}
--- carproject_reinforcementlearning/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ carproject_reinforcementlearning/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -8,7 +8,9 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-
+        private float horizontal = 0;
+        private float vertical = 0;
+   
 
         private void Awake()
         {
@@ -16,17 +18,27 @@ namespace UnityStandardAssets.Vehicles.Car
             m_Car = GetComponent<CarController>();
         }
 
+        public void setHorizontal(float horizontal)
+        {
+            this.horizontal = horizontal;
+        }
+
+        public void setVertical(float vertical)
+        {
+            this.vertical = vertical;
+        }
 
         private void FixedUpdate()
         {
             // pass the input to the car!
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            //float v = CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
+            
+            m_Car.Move(horizontal, vertical, vertical, handbrake);
 #else
-            m_Car.Move(h, v, v, 0f);
+            m_Car.Move(horizontal, vertical, vertical, 0f);
 #endif
         }
     }
