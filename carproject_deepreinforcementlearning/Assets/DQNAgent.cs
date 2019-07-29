584c582182ccfc6153bd8e4070c758f2aca42b65
andrea.pollastro <andrea.poll93@gmail.com>
2019-07-30 00:39:18 +0200

    Deep-Q-learning project started

---
+++ carproject_deepreinforcementlearning/Assets/DQNAgent.cs
@@ -0,0 +1,89 @@
+﻿using MLAgents;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+using System.Collections;
+
+public class DQNAgent : Agent
+{
+    private Rigidbody rigidbody;
+    private CarUserControl carUserControl;
+    private RaycastController raycastController;
+    private bool collided;
+    //state
+    private float[] state = new float[RaycastController.numRays];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting components for driving the car
+        carUserControl = GetComponent<CarUserControl>();
+        rigidbody = GetComponent<Rigidbody>();
+        raycastController = GetComponent<RaycastController>();
+    }
+
+    public override void AgentReset()
+    {
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        raycastController.resetCurrentState();
+        collided = false;
+    }
+
+    public override void CollectObservations()
+    {
+        //raystate
+        AddVectorObs(state);
+        //car's velocity
+        AddVectorObs(transform.InverseTransformDirection(rigidbody.velocity).z);
+    }
+
+    private void performAction(float steeringAngle, float acceleration)
+    {
+        carUserControl.setHorizontal(steeringAngle);
+        carUserControl.setVertical(acceleration);
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        float reward;
+        float steeringAngle = vectorAction[0];
+        float acceleration = vectorAction[1];
+        float accelerationScore;
+        float steeringScore;
+
+        performAction(steeringAngle, acceleration);
+        //reward calculation
+        accelerationScore = acceleration * getRaysScore(); //TODO: controlla se servono solo i centrali
+        steeringScore = getSteeringScore(steeringAngle, acceleration);
+        reward = collided ? -1000 : accelerationScore + steeringScore;
+
+        SetReward(reward);
+        if (collided)
+            Done();
+    }
+
+    private float getRaysScore()
+    {
+        float res = 0;
+        for (int i = 0; i < state.Length; i++)
+            res += state[i];
+        return res;
+    }
+    
+    private float getSteeringScore(float steeringAngle, float acceleration)
+    {
+        float res = 0;
+        int startPoint = (steeringAngle * acceleration) < 0 ? 0 : state.Length / 2;
+        int endPoint = (steeringAngle * acceleration) < 0 ? state.Length/2 : state.Length;
+        for (int i = startPoint; i < endPoint; i++)
+            res += state[i];
+        return res;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+    }
+}
---
+++ carproject_deepreinforcementlearning/Assets/Scripts/RaycastController.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+public class RaycastController : MonoBehaviour
+{
+    //Objects needed for the rays' description and raycasting
+    public static int numRays = 17;
+    private float[] lengthRay = new float[numRays];
+    private Ray[] rays = new Ray[numRays];
+    private RaycastHit hit;
+
+    //Physics controller
+    private Rigidbody rigidbody;
+    private Vector3 localVelocity;
+    private bool collided = false;
+
+    //Current state
+    private float[] state = new float[numRays];
+
+
+    void Start()
+    {
+        initState();
+        defineRayLength();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void defineRayLength()
+    {
+        int halfRays = numRays / 2;
+        float maxLength = 35f;
+        float minLength = 15f;
+        float diff = (maxLength - minLength) / (halfRays + 1);
+
+        lengthRay[halfRays] = 30f;
+        for (int i = 1; i < halfRays; i++)
+            lengthRay[halfRays + i] = lengthRay[halfRays - i] = lengthRay[halfRays] - (diff * i);
+    }
+
+    private void initState()
+    {
+        for(int i = 0; i < state.Length; i++)
+            state[i] = 1;
+    }
+
+    //callback pre-frame
+    void FixedUpdate()
+    {
+        if (!collided)
+        {
+            //Angle coverage: 120 degrees
+            int angle = -40;
+
+            //Rays creation
+            for (byte i = 0; i < numRays; i++)
+            {
+                rays[i] = new Ray(transform.position, Quaternion.AngleAxis(angle, Vector3.up) * transform.forward);
+                angle += 5;
+            }
+
+            //Rays drawing
+            for (byte i = 0; i < numRays; i++)
+                Debug.DrawRay(rays[i].origin, rays[i].direction * lengthRay[i], Color.green);
+
+            //Raycasting
+            for (byte i = 0; i < numRays; i++)
+            {
+                if (Physics.Raycast(rays[i], out hit, lengthRay[i]))
+                {
+                    Vector3 res = hit.point - transform.position;
+                    state[i] = transformHitPoint(res.magnitude, lengthRay[i]);
+                    Debug.DrawLine(transform.position, hit.point, Color.HSVToRGB(0, 1, res.magnitude/lengthRay[i]));
+                }
+            }
+        }
+    }
+
+    private float transformHitPoint(float k, float x)
+    {
+        //Codomain: [-1,1]
+        return 2 * (k / x) - 1;
+    }
+
+    public void resetCurrentState()
+    {
+        initState();
+        collided = false;
+    }
+
+    public byte[] getCurrentState()
+    {
+        return (byte[])state.Clone();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collided = true;
+    }
+}
